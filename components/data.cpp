ae9e53f7a2a6886fe0c7ae7931f5f33a41b3942d
Prit Kanadiya <103832825+PritK99@users.noreply.github.com>
2024-01-15 19:04:29 +0530

    Adding implementation for open vocabulary

--- components/data.cpp
+++ components/data.cpp
@@ -10,6 +10,7 @@
 
 using namespace std;
 
+// custom hash function for using pair as a key in hashmap
 struct pair_hash 
 {
     template <typename T1, typename T2>
@@ -54,6 +55,13 @@ public:
     void calculate_probs();
 };
 
+/*
+* Function Name: load_dataset
+* Input: None
+* Output: None
+* Logic: Opens a file specified by the 'path' member variable and processes its content line by line. Each line contains a word and its corresponding part of speech. This also populates the pairs vector, which holds the word and its corresponding POS.
+* Example Call: dataset.load_dataset();
+*/
 void Dataset::load_dataset()
 {
     ifstream dataset;
@@ -87,6 +95,13 @@ void Dataset::load_dataset()
     dataset.close();
 }
 
+/*
+* Function Name: process_line
+* Input: A string 'line' representing a line from the dataset file
+* Output: A pair of strings representing two words obtained from the input line
+* Logic: Processes the input line to extract two words separated by a tab character ('\t'). The extracted words are then returned as a pair. If the input line is empty, a special pair ("--s--", "--s--") representing the start tag is returned.
+* Example Call: pair<string, string> result = dataset.process_line("word1\tword2");
+*/
 pair<string, string> Dataset::process_line(string line)
 {
     if (line.length() == 0)
@@ -110,8 +125,17 @@ pair<string, string> Dataset::process_line(string line)
     return make_pair(word1, word2);
 }
 
+/*
+* Function Name: create_vocabulary
+* Input: None
+* Output: None
+* Logic: Processes the pairs of strings in the 'pairs' vector member variable to create a vocabulary. It counts the frequency of each word, inserts corresponding part-of-speech (POS) tags into the 'POS' set member variable, and filters out words with a frequency less than or equal to 1. Words that meet the frequency criterion are added to the 'vocab' set member variable. If a word does not meet the frequency criterion, its corresponding word in the pair is set to "UNK". Finally, "UNK" is added to the vocabulary.
+* Example Call: dataset.create_vocabulary();
+*/
 void Dataset::create_vocabulary()
 {
+    unordered_map <string, int> vocab_freq;
+
     for (auto p : pairs)
     {
         if (p.first == "--s--")
@@ -119,17 +143,41 @@ void Dataset::create_vocabulary()
             // do nothing for start tag
             continue;
         }
-        vocab.insert(p.first);
+        vocab_freq[p.first]++;
         POS.insert(p.second);
     }
-    
-    // adding unknown tag to vocab to represent unknown words
+
+    for (auto p : pairs)
+    {
+        if (p.first == "--s--")
+        {
+            // do nothing for start tag
+            continue;
+        }
+        else if (vocab_freq[p.first] > 1)
+        {
+            vocab.insert(p.first);
+        }
+        else
+        {
+            p.first = "UNK";
+        }
+        vocab_freq[p.first]++;
+    }
+
     vocab.insert("UNK");
 
     cout << "There are " << vocab.size() << " Unique words in dataset" << endl;
     cout << "There are " << POS.size() << " Unique POS tags in dataset" << endl;
 }
 
+/*
+* Function Name: count_frequencies
+* Input: None
+* Output: None
+* Logic: Iterates through pairs of strings in the 'pairs' vector member variable to count frequencies for transition, emission, and prior probabilities. For each pair of consecutive words, it updates the transition frequency in the 'transition_freq' unordered_map. It also updates the emission frequency for the current word and its corresponding POS tag in the 'emission_freq' unordered_map, and increments the frequency of the POS tag in the 'tag_freq' unordered_map. Additionally, it handles the special case when the current word is preceded by the start tag "--s--", updating the prior frequency in the 'prior_freq' unordered_map and incrementing the prior tag frequency in the 'prior_tag_freq' unordered_map.
+* Example Call: dataset.count_frequencies();
+*/
 void Dataset::count_frequencies()
 {
     bool isStart = false;
@@ -159,6 +207,13 @@ void Dataset::count_frequencies()
     }
 }
 
+/*
+* Function Name: calculate_probs
+* Input: None
+* Output: None
+* Logic: Calculates logarithmic probabilities for transition, emission, and prior probabilities based on the previously computed frequencies. For each entry in 'transition_freq', it calculates the transition probability and stores it in 'transition_probs'. Similarly, it calculates the emission probabilities for each entry in 'emission_freq' and stores them in 'emission_probs'. For the prior probabilities, it computes the logarithmic value and updates 'prior_probs' using the frequencies in 'prior_freq' and 'prior_tag_freq'.
+* Example Call: dataset.calculate_probs();
+*/
 void Dataset::calculate_probs()
 {
     for (auto &entry : transition_freq)
